220f43292109383df0cf1f591362e230aaf39008
lucasdelmonte <delmontelucas678@gmail.com>
2021-06-18 13:30:50 -0300

    Se agrego un menu desplegable para el nav y se mejoro el diseño responsive de la galeria

--- js/app.js
+++ js/app.js
@@ -10,3 +10,27 @@ function handleSubmit(event){
     $buttonMailto.click();
     document.getElementById("form").reset();
 }
+
+
+var $btnMenu = document.querySelector('img[alt="Menu"]'), desplegado = true;
+
+$btnMenu.addEventListener('click', menuDesplegable);
+
+
+function menuDesplegable() {
+    if (desplegado) {
+        $enlace = document.querySelectorAll('.enlace');
+    
+        for (var i = 0; i < $enlace.length; i++) {
+            $enlace[i].classList.remove('oculto');
+            $enlace[i].classList.add('mostrado');
+        } 
+        desplegado = false;
+    } else {
+        for (var i = 0; i < $enlace.length; i++) {
+            $enlace[i].classList.remove('mostrado');
+            $enlace[i].classList.add('oculto');
+        } 
+        desplegado = true;
+    }
+}

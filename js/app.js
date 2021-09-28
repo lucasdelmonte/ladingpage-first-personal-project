7460d68dc64c94802d58638923a50c189742c285
lucasdelmonte <delmontelucas678@gmail.com>
2021-09-28 20:57:13 -0300

    Cambios importantes en los colores de la pagina.

--- js/app.js
+++ js/app.js
@@ -1,36 +1,40 @@
-const $form = document.querySelector('#form');
-const $buttonMailto = document.querySelector('#fundamental');
+const $form = document.querySelector("#form");
+const $buttonMailto = document.querySelector("#fundamental");
 
-$form.addEventListener('submit', handleSubmit);
+$form.addEventListener("submit", handleSubmit);
 
-function handleSubmit(event){
-    event.preventDefault();
-    const form = new FormData(this);
-    $buttonMailto.setAttribute('href', `mailto:teraflop.computacion@gmail.com?subject=Nombre: ${form.get('nombre')} - Asunto: ${form.get('asunto')}&body=${form.get('mensaje')}`);
-    $buttonMailto.click();
-    document.getElementById("form").reset();
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = new FormData(this);
+  $buttonMailto.setAttribute(
+    "href",
+    `mailto:${form.get("email")}?subject=Nombre: ${form.get("nombre")} - 
+    Asunto: ${form.get("asunto")}&body=${form.get("mensaje")}`
+  );
+  $buttonMailto.click();
+  document.getElementById("form").reset();
 }
 
+// Menu desplegable
+var $btnMenu = document.querySelector('img[alt="Menu"]'),
+  desplegado = true;
 
-var $btnMenu = document.querySelector('img[alt="Menu"]'), desplegado = true;
-
-$btnMenu.addEventListener('click', menuDesplegable);
-
+$btnMenu.addEventListener("click", menuDesplegable);
 
 function menuDesplegable() {
-    if (desplegado) {
-        $enlace = document.querySelectorAll('.enlace');
-    
-        for (var i = 0; i < $enlace.length; i++) {
-            $enlace[i].classList.remove('oculto');
-            $enlace[i].classList.add('mostrado');
-        } 
-        desplegado = false;
-    } else {
-        for (var i = 0; i < $enlace.length; i++) {
-            $enlace[i].classList.remove('mostrado');
-            $enlace[i].classList.add('oculto');
-        } 
-        desplegado = true;
+  if (desplegado) {
+    $enlace = document.querySelectorAll(".enlace");
+
+    for (var i = 0; i < $enlace.length; i++) {
+      $enlace[i].classList.remove("oculto");
+      $enlace[i].classList.add("mostrado");
     }
+    desplegado = false;
+  } else {
+    for (var i = 0; i < $enlace.length; i++) {
+      $enlace[i].classList.remove("mostrado");
+      $enlace[i].classList.add("oculto");
+    }
+    desplegado = true;
+  }
 }
